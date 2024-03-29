87498d39ad50ff89d2326d2eb5712f50cc3c9de0
Utkarsh Tiwari <utk0703@gmail.com>
2024-03-29 19:07:16 +0530

    responsive

--- script.js
+++ script.js
@@ -535,4 +535,62 @@ music.addEventListener('ended',()=>{
   }  
 });
 
+// Search box
+
+let search_results = document.getElementsByClassName('search_results')[0];
+
+songs.forEach((element)=>{
+    const {id,songName,poster} = element;
+    let card = document.createElement('a');
+    card.classList.add('card');
+    card.href = "#"+id;
+    card.innerHTML = `
+    <img src="${poster}" alt="">
+    <div class="content">
+        ${songName}
+    </div>`;
+    search_results.appendChild(card);
+})
+
+let input = document.getElementsByTagName('input')[0];
+input.addEventListener('keyup' , ()=>{
+    let input_value = input.value.toUpperCase();
+    let items = search_results.getElementsByTagName('a');
+
+    for(let i = 0;i < items.length ; i++)
+    {
+        let as = items[i].getElementsByClassName('content')[0];
+        let text_value = as.textContent || as.innerHTML;
+
+        if(text_value.toUpperCase().indexOf(input_value) > -1){
+            items[i].style.display = "flex";
+        }
+        else{
+            items[i].style.display = "none";
+        }
+
+        if(input.value == 0){
+            search_results.style.display = "none";
+        }
+        else{
+            search_results.style.display = "";
+        }
+    }
+})
+
+let menu_list_active_button = document.getElementById('menu_list_active_button');
+
+let menu_side = document.getElementsByClassName('menu_side')[0];
+
+menu_list_active_button.addEventListener('click',()=>{
+    menu_side.style.transform = "unset";
+    menu_list_active_button.style.opacity = 0;
+})
+
+let song_side = document.getElementsByClassName('song_side')[0];
+
+song_side.addEventListener('click',()=>{
+    menu_side.style.transform = "translateX(-100%)";
+    menu_list_active_button.style.opacity = 1;
+})
 
