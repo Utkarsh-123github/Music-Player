3d640fa8dafb129dc152d8e23004741944358b36
Utkarsh Tiwari <utk0703@gmail.com>
2024-03-29 19:25:14 +0530

    modify

--- script.js
+++ script.js
@@ -583,14 +583,15 @@ let menu_list_active_button = document.getElementById('menu_list_active_button')
 let menu_side = document.getElementsByClassName('menu_side')[0];
 
 menu_list_active_button.addEventListener('click',()=>{
-    menu_side.style.transform = "unset";
-    menu_list_active_button.style.opacity = 0;
+    if(menu_side.style.transform == "unset"){
+        menu_side.style.transform = "translateX(-100%)"
+        menu_list_active_button.style.opacity = 1;
+    }
+    else{
+        menu_side.style.transform = "unset";
+        menu_list_active_button.style.opacity = 0.5;
+    }
 })
 
-let song_side = document.getElementsByClassName('song_side')[0];
 
-song_side.addEventListener('click',()=>{
-    menu_side.style.transform = "translateX(-100%)";
-    menu_list_active_button.style.opacity = 1;
-})
 
